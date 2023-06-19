4b0d4a77232012c6ef1c24c29505c539f88f30fd
gerardopanero <gerardo_panero@hotmail.com>
2023-06-18 22:30:43 -0300

    render lo mas

--- src/test/java/com/portfolioweb/gmp/GmpApplicationTests.java
+++ src/test/java/com/portfolioweb/gmp/GmpApplicationTests.java
@@ -1,13 +1,7 @@
 package com.portfolioweb.gmp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GmpApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
